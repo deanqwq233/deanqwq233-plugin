00bddab8b991f71039e00e466664b7ba3e233e87
deanqwq233 <dean_qwq@outlook.com>
2024-02-06 19:25:36 +0800

    240206follow6

--- apps/follow6.js
+++ apps/follow6.js
@@ -1,51 +1,35 @@
-﻿import plugin from '../../../lib/plugins/plugin.js'
-import fs from 'fs';
-import yaml from 'js-yaml';
-
-export class example extends plugin {
-    constructor() {
-        super({
-            name: '遇到6随机往后接不超过5位',
-            dsc: '遇到6随机往后接不超过5位',
-            event: 'message',
-            priority: 5000,
-            rule: [
-                {
-                    reg: '^6$',
-                    fnc: 'liu'
-                },
-            ]
-        })
-
-        // 读取并解析 setting.system.yaml 文件  
-        try {
-            const file = fs.readFileSync('./defSet/defult/setting.system.yaml', 'utf8');
-            const data = yaml.load(file);
-
-            // 判断 follow6 的值  
-            if (data.follow6 === true) {
-                this.follow6 = true;
-            } else {
-                this.follow6 = false;
+﻿//一定是个制杖写的
+//你看他不写注释，这种人，那什么东西啊(
+    import plugin from '../../lib/plugins/plugin.js'
+    import { segment } from "oicq";
+    export class example extends plugin {
+        constructor() {
+            super({
+                name: '遇到6随机往后接不超过5位',
+                dsc: '遇到6随机往后接不超过5位',
+                event: 'message',
+                priority: 5000,
+                rule: [
+                    {
+                        reg: '^6$',
+                        fnc: 'liu'
+                    },
+                ]
+            })
+        }
+        async liu(e) {
+            let n = Math.floor(Math.random() * 5 + 1)
+            let msg = ''
+            let i = 0;
+            for (i = 1; i <= n; i++) {
+                let send = i + 6;
+                    msg = [`${send}`]
+                await this.reply(msg)
+                await sleep(1500);
             }
-        } catch (err) {
-            console.error(err);
+            return true
         }
     }
-    async liu(e) {
-        if (!this.follow6) return; // 如果 follow6 为 false，则直接返回，不执行下面的代码  
-        let n = Math.floor(Math.random() * 5 + 1)
-        let msg = ''
-        let i = 0;
-        for (i = 1; i <= n; i++) {
-            let send = i + 6;
-            msg = [`${send}`]
-            await this.reply(msg)
-            await sleep(1500);
-        }
-        return true
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
     }
-}
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
